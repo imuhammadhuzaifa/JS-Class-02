51cab44c3502b796224e2431b6b1feee5a665b32
iMuhammadHuzaifa <itshuzaifa09@gmail.com>
2025-02-15 02:08:55 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// var a = 7;
+
+// var b = 8;
+
+// var c = a+b;
+
+// console.log(c);
+
+
+// var e = 9;
+
+// var f = 10;
+
+// var g = e-f;
+
+// console.log(g);
+
+
+// var h = "Huzaifa";
+
+// var i = 10;
+
+// var j = h*i;
+
+// console.log(j);
+
+
+// var k = "20";
+
+// var l = 10;
+
+// var m = k-l;
+
+// console.log(m);
+
+
+// var n = 9/3+4-6*2;
+
+// console.log(n);
+
+// var o = 9/3+(4-6)*2;
+
+// console.log(o);
+
+
+// var a = 2;
+
+// var b = --a + a + a++ - a-- + a + --a + a-- - a++ - --a;
+
+// console.log(b);
+
+
+// var a = 4;
+
+// var b = 2;
+
+// var exp = a - --b + ++a - --a - --b + a;
+
+//        4 - 1 + 5 - 4 - 0 + 4
+
+// console.log(exp);
+
+
+// var li1 = "List1";
+
+// var li2 = "List2";
+
+// var li3 = "List3";
+
+// document.write("<ul><li>", li1, "</li><li>", li2, "</li><li>", li3, "</li></ul>");
+
+
+// var firstName = prompt("Enter first name");
+
+// var lastName = prompt("Enter last name");
+
+// console.log("My name is", firstName, lastName);
+
+
+// var firstNumber = +prompt("Enter first number");
+
+// var lastNumber = +prompt("Enter last number");
+
+// console.log(firstNumber + lastNumber);
